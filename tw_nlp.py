260b05cf12236d4152321b37fd16604f27f007c4
Your Name <norikmkrt@mail.ru>
2020-06-20 23:37:12 +0300

    tw

--- tw_nlp.py
+++ tw_nlp.py
@@ -1,7 +1,12 @@
 import textblob
 import nltk
 from textblob import TextBlob
-text='привет, я красавчик Олег, любите меня нежно'
+text='привет, я красавчик Олег, любите меня нежно.Я пою и играю на гитаре.'
 blob=TextBlob(text)
-print( blob.tags)
+tb_tags=blob.tags
+tb_words=blob.words
+tb_sent=blob.sentences
+tb_ser=blob.serialized
+
+print( tb_ser)
 
